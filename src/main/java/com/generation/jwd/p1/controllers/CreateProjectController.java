f3f59376c31f35988b1c6a54b863113ed5934441
Daniel Lozano <lozanodaniel7@gmail.com>
2018-02-07 23:48:16 +0100

    Created createproject

--- src/main/java/com/generation/jwd/p1/controllers/CreateProjectController.java
+++ src/main/java/com/generation/jwd/p1/controllers/CreateProjectController.java
@@ -10,6 +10,9 @@ import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.generation.jwd.p1.beans.Project;
 
 @Controller
 public class CreateProjectController {
@@ -19,7 +22,41 @@ public class CreateProjectController {
 	public String addProject() {
 		
 		System.out.println("project get");
-		return null;
+		return "createproject";
 	}
 	
+	@RequestMapping(method = RequestMethod.POST)
+	public String submitProject(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam("name") String name,
+			@RequestParam("desc") String desc,
+			@RequestParam("notes") String notes,
+			@RequestParam("dateBegin") String dateBegin,
+			@RequestParam("dateEnd") String dateEnd,
+			@RequestParam("status") String status){
+		
+		System.out.println("catched data");
+		Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction t = (Transaction) session.beginTransaction();
+        
+        Project createproject = new Project();
+        createproject.setName(name);
+        createproject.setDesc(desc);
+        createproject.setNotes(notes);
+        createproject.setDateBegin(dateBegin);
+        createproject.setDateEnd(dateEnd);
+        createproject.setStatus(status);
+        
+        System.out.println("after setting");
+        
+		session.persist(createproject);
+		session.getTransaction().commit();
+		session.close();
+		
+				return "homeuser";
+	
+	}
 }
