29fc113d1ea0144d8f87a6b7674db406a7bece79
danielooloza <lozanodaniel7@gmail.com>
2018-02-07 17:27:11 +0100

    Created controller

--- src/main/java/com/generation/jwd/p1/controllers/CreateProjectController.java
+++ src/main/java/com/generation/jwd/p1/controllers/CreateProjectController.java
@@ -1,5 +1,25 @@
 package com.generation.jwd.p1.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class CreateProjectController {
 
+	@RequestMapping(value = "/project", method = RequestMethod.GET)
+	
+	public String addProject() {
+		
+		System.out.println("project get");
+		return null;
+	}
+	
 }
