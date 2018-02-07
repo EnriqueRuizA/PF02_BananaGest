1111370411942c7a8895ccee52fa3c347c5d5b0e
danielooloza <lozanodaniel7@gmail.com>
2018-02-07 09:47:00 +0100

    user for hibernate

--- src/main/java/com/generation/jwd/p1/beans/User.java
+++ src/main/java/com/generation/jwd/p1/beans/User.java
@@ -1,64 +1,77 @@
 package com.generation.jwd.p1.beans;
 import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User implements Serializable {
     
     private static final long serialVersionUID = 1L;
-    private int id_user;
-    private String name_user;
-    private String surname_user;
-    private String email_user;
-    private String password_user;
+   
+    @Id @GeneratedValue
+    @Column(name="id")
+    private int id;
+    
+    @Column(name="name")
+    private String name;
+    
+    @Column(name="surname")
+    private String surname;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column(name="password")
+    private String password;
     
     public User() {
     }
-    
-    public User(int id_user, String name_user, String surname_user, String email_user, String password_user){
-        this.id_user = id_user;
-        this.name_user = name_user;
-        this.surname_user = surname_user;
-        this.email_user = email_user;
-        this.password_user = password_user;
-    }
-    
-	public int getId_user() {
-		return id_user;
-	}
-	
-	public void setId_user(int id_user) {
-		this.id_user = id_user;
-	}
-	
-	public String getName_user() {
-		return name_user;
-	}
-	
-	public void setName_user(String name_user) {
-		this.name_user = name_user;
-	}
-	
-	public String getSurname_user() {
-		return surname_user;
-	}
-	
-	public void setSurname_user(String surname_user) {
-		this.surname_user = surname_user;
-	}
-	
-	public String getEmail_user() {
-		return email_user;
-	}
-	
-	public void setEmail_user(String email_user) {
-		this.email_user = email_user;
-	}
-	
-	public String getPassword_user() {
-		return password_user;
-	}
-	
-	public void setPassword_user(String password_user) {
-		this.password_user = password_user;
+
+	public int getId() {
+		return id;
 	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
 }
 
